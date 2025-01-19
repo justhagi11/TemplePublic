2a9544261f89bd13acc3abccd52e6c278350fee9
hi <hagifx001@yahoo.com>
2025-01-19 10:33:05 +0100

    proper connector, basic shit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+import ConnectDatabase from "./utilites/database";
+import logger from "./utilites/logger";
+const app = new Hono();
+export default app; // so we can use it in routes n shit
+
+const port = 3551;
+Bun.serve({
+    fetch: app.fetch,
+    port: port,
+    development: false,
+}); //serve with bun
+
+logger.info(`TemplePublic listening on ${port}`);
+await ConnectDatabase();
---
+++ src/utilites/database.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import logger from "./logger";
+dotenv.config();
+
+const mongourl: string = process.env.MONGODB as string;
+
+
+function ConnectDatabase(){
+    try{
+        mongoose.connect(mongourl);
+        logger.info('Successfully connected to your database.')
+    }catch(error){logger.error(`There was an error while connecting to your database: ${error}` 
+        // i think it should catch!!!???!
+    )}
+};
+export default ConnectDatabase;
